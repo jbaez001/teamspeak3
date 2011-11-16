ff1d900c1ec8c7ef4b1fec2bf345f360ef059062
Juan Baez <jbaez001@gmail.com>
2011-11-15 21:01:45 -0600

    Added the "read" method and some comments here and there.

--- teamspeak3.py
+++ teamspeak3.py
@@ -31,6 +31,14 @@ class teamspeak:
   default_timeout_read      = 5
   
   def encode_string(self, string):
+    """
+    Encodes a string
+    
+    @param string: String to encode
+    @type string: str
+    
+    @return: Encoded string
+    """
     string = string.replace('\\', '\\\\')
     string = string.replace('/',  '\\/')
     string = string.replace(' ',  '\\s')
@@ -46,6 +54,14 @@ class teamspeak:
     return string
 
   def decode_string(self, string):
+    """
+    Decodes a string
+    
+    @param string: String to decode
+    @type string: str
+    
+    @return: Secoded string
+    """
     string = string.replace('\\\\', '\\')
     string = string.replace('\\/',  '/')
     string = string.replace('\\s',  ' ')
@@ -61,14 +77,19 @@ class teamspeak:
     return string
 
   def __init__(self, server_host, server_port='10011'):
+    """
+    Class constructor
+    """
     self.server_port  = server_port
     self.server_host  = server_host
-    self.connected    = False
-    
-  def is_connected(self):
-    return self.connected
   
   def connect(self, server_id=None):
+    """
+    Connects to the server
+    
+    @param server_id: Virtual Server's ID
+    @type server_id: string 
+    """
     data = ''
     
     try:
@@ -89,10 +110,49 @@ class teamspeak:
     self.connected = True
     return True
   
+  def read(self):
+    """
+    Reads from the server. This is useful for server notifications/events.
+    
+    @return: a list of data to parse
+    @return: None if there is nothing to parse
+    """
+    data = self.net.read_very_eager()
+      
+    if len(data) > 1:
+      data = data.split('\n')
+      return data
+    
+    return None
+      
+  def disconnect(self):
+    """
+    Disconnects from the server.
+    """
+    self.net.write('quit\n')
+    self.net.close()
+    
   def login(self, client_login_name, client_login_password):
+    """
+    Logins to the server.
+    
+    @param client_login_name: Server query login
+    @type client_login_name: str
+    
+    @param client_login_password: Server query password
+    @type client_login_password: str
+    """
     return self.command('login', { 'client_login_name' : client_login_name, 'client_login_password' : client_login_password})
     
   def parse_results(self, res):
+    """
+    Experimental parser.
+    
+    @param res: Raw data.
+    @type res: str
+    
+    @return: dictionary of parsed items
+    """
     out = {}
     
     res = res.replace('\r', '')
@@ -107,12 +167,26 @@ class teamspeak:
     return out
   
   def command(self, cmd, parameters={}, options=''):
+    """
+    Executes a command.
+    
+    @param cmd: Command to execute.
+    @type cmd: str
+    
+    @param parameters: Parameters
+    @type parameters: dict
+    
+    @param options: Options to pass.
+    @type options: str
+    
+    @return: a list of dictionaries containing the results
+    """
     ret = []
     
     out = cmd
     
     for key in parameters:
-      out += ' %s=%s' % (key, parameters[key])
+      out += ' %s=%s' % (key, self.encode_string(parameters[key]))
     
     out += ' ' + options + '\n'
     
@@ -126,7 +200,7 @@ class teamspeak:
     error_msg = results[len(results)-2].split(' ')[2].split('=')[1]
     
     if (error_id != '0'):
-      print 'Server Error: %s' % error_msg
+      print 'Server Error: %s, on command: %s' % (error_msg, cmd)
       return ret
       
     results = results[0]
@@ -141,7 +215,23 @@ class teamspeak:
     return ret
   
   def get_client_list(self, options=''):
+    """
+    Retrieves a list of clients with the given options.
+    
+    @param options: Options.
+    @type options: str
+    
+    @return: Same as command
+    """
     return self.command('clientlist', {}, options)
   
   def get_client_info(self, clid):
+    """
+    Retrieves information for a specific client.
+    
+    @param clid: Client's Id
+    @type clid: str
+    
+    @return: Same as command
+    """
     return self.command('clientinfo', {'clid' : clid})
